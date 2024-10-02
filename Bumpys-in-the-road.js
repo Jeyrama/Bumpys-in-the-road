eb05ade9bc666239393316986a4f968b03f1cbac
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-10-02 19:52:29 +0200

    Update Bumpys-in-the-road.js

--- Bumpys-in-the-road.js
+++ Bumpys-in-the-road.js
@@ -7,3 +7,6 @@ Given a string showing either flat road (_) or bumps (n).
 If you are able to reach home safely by encountering 15 bumps or less, 
 return Woohoo!, otherwise return Car Dead
 */
+
+
+// Solution
