8bfdfa69009f697ea5a5cf2b5fff8ff4f88d45ab
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-10-02 19:53:13 +0200

    Update Bumpys-in-the-road.js

--- Bumpys-in-the-road.js
+++ Bumpys-in-the-road.js
@@ -10,3 +10,17 @@ return Woohoo!, otherwise return Car Dead
 
 
 // Solution
+
+const bump = x => {
+  let string = "";
+  for (let count = 0; count < x.length; count++) {
+    if (x[count] == 'n') {
+      string += x[count];
+    }
+  }
+  if (string.length > 15) {
+    return 'Car Dead';
+  } else {
+    return 'Woohoo!'
+  }
+}
